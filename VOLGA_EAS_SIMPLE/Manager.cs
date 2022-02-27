23318bd9914d8191f191bc9b6b67878f0e8e182b
Mod3lLAPTOP <lapikov.2003@mail.ru>
2022-02-27 16:36:47 +0300

    Содание упрощенного проекта VOLGA_EAS_SIMPLE

---
+++ VOLGA_EAS_SIMPLE/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace VOLGA_EAS_SIMPLE
+{
+    class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
