24a6111049e458b4d00fc13c1bd5f067853182c3
Mod3lLAPTOP <lapikov.2003@mail.ru>
2022-03-27 20:42:04 +0300

    g

--- VOLGA_EAS_SIMPLE/Pages/LoginPage.xaml.cs
+++ VOLGA_EAS_SIMPLE/Pages/LoginPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,8 @@ namespace VOLGA_EAS_SIMPLE.Pages
     /// </summary>
     public partial class LoginPage : Page
     {
+        private USERS _selectedUser = new USERS();
+
         public LoginPage()
         {
             InitializeComponent();
@@ -27,7 +31,88 @@ namespace VOLGA_EAS_SIMPLE.Pages
 
         private void Login_Click(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new MainPage());
+            //StringBuilder errors = new StringBuilder();
+
+            //if (string.IsNullOrWhiteSpace(_selectedUser.USER_NAME))
+            //    errors.AppendLine("Укажите название проекта");
+
+            //if (string.IsNullOrWhiteSpace(_selectedUser.USER_PASSWORD))
+            //    errors.AppendLine("Укажите описание проекта");
+
+            //if (errors.Length > 0)
+            //{
+            //    MessageBox.Show(errors.ToString());
+            //    return;
+            //}
+
+            SqlConnection connection1 = new SqlConnection("Data Source=DESKTOP-5CU2HHD;Initial Catalog=VOLGA_EAS_DB;Integrated Security=True");
+            SqlCommand comand1 = new SqlCommand();
+            SqlDataAdapter adaptor1 = new SqlDataAdapter();
+            DataSet dataset1 = new DataSet();
+            DataTable dt = new DataTable();
+
+            comand1.CommandText = @"SELECT ""USER_NAME"", ""USER_PASSWORD"" FROM ""USERS"" WHERE ""USER_NAME""='" + Loging.Text +
+            @"'AND ""USER_PASSWORD""='" + Passwordg.Text + "'";
+
+
+            comand1.Connection = connection1;
+
+            adaptor1.SelectCommand = comand1;
+            adaptor1.Fill(dataset1, "0");
+            adaptor1.Fill(dt);
+            int count1 = dataset1.Tables[0].Rows.Count;
+            if (count1 > 0)
+            {
+                connection1.Close();
+                Manager.MainFrame.Navigate(new MainPage());
+            }
+            else
+            {
+                MessageBox.Show("Не правильный логин или пароль");
+                Loging.Clear();
+                Passwordg.Clear();
+            }
+            //try
+            //{
+            //    VOLGA_EAS_DBEntities.GetContext().SaveChanges();
+            //    MessageBox.Show("Информация сохранена");
+            //    Manager.MainFrame.GoBack();
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message.ToString());
+            //}
+
+            //SqlConnection connection1 = new SqlConnection();
+            //connection1.ConnectionString = Properties.Settings.Default.qwertyConnectionString1;
+            //SqlCommand comand1 = new SqlCommand();
+            //SqlDataAdapter adaptor1 = new SqlDataAdapter();
+            //DataSet dataset1 = new DataSet();
+            //DataTable Manager.MainFrame.Navigate(new MainPage());
+            //SqlC dt = new DataTable();
+
+            //comand1.CommandText = @"SELECT ""id"", ""name"" FROM ""Table_1"" WHERE ""id""='" + tb1.Text +
+            //@"'AND ""Password""='" + tb2.Text + "'";
+
+            //comand1.Connection = connection1;
+
+            //adaptor1.SelectCommand = comand1;
+            //adaptor1.Fill(dataset1, "0");
+            //adaptor1.Fill(dt);
+            //int count1 = dataset1.Tables[0].Rows.Count;
+            //if (count1 > 0)
+            //{
+            //    connection1.Close();
+            //    MainWindow Menu = new MainWindow();
+            //    Menu.Show();
+            //    this.Hide();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Не правильный логин или пароль");
+            //    tb1.Clear();
+            //    tb2.Clear();
+            //}
         }
 
         private void Registration_Click(object sender, RoutedEventArgs e)
--- VOLGA_EAS_SIMPLE/Pages/RegistrationPage.xaml.cs
+++ VOLGA_EAS_SIMPLE/Pages/RegistrationPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,47 +22,74 @@ namespace VOLGA_EAS_SIMPLE.Pages
     /// </summary>
     public partial class RegistrationPage : Page
     {
-        private USERS _currentUser = new USERS();
-        private string ConfPass;
+        //private USERS _currentUser = new USERS();
 
         public RegistrationPage()
         {
             InitializeComponent();
-            this.DataContext = new VOLGA_EAS_DBEntities();
+            //this.DataContext = new VOLGA_EAS_DBEntities();
         }
 
         private void Login_Click(object sender, RoutedEventArgs e)
         {
-            StringBuilder errors = new StringBuilder();
+            //private string ConfPass = ConffPasswordr.Text;
+            //SqlConnection connection1 = new SqlConnection("Data Source=DESKTOP-5CU2HHD;Initial Catalog=VOLGA_EAS_DB;Integrated Security=True");
+            //SqlCommand comand1 = new SqlCommand();
+            //SqlDataAdapter adaptor1 = new SqlDataAdapter();
+            //DataSet dataset1 = new DataSet();
+            //DataTable dt = new DataTable();
 
-            if (string.IsNullOrWhiteSpace(_currentUser.USER_EMAIL))
-                errors.AppendLine("Укажите название электронную почту");
+            //comand1.CommandText = @"SELECT ""USER_NAME"", ""USER_PASSWORD"" FROM ""USERS"" WHERE ""USER_NAME""='" + Loging.Text +
+            //@"'AND ""USER_PASSWORD""='" + Passwordg.Text + "'";
 
-            if (string.IsNullOrWhiteSpace(_currentUser.USER_NAME))
-                errors.AppendLine("Укажите никнейм");
 
-            if (string.IsNullOrWhiteSpace(_currentUser.USER_PASSWORD))
-                errors.AppendLine("Укажите пароль");
+            //comand1.Connection = connection1;
 
-            if (errors.Length > 0)
-            {
-                MessageBox.Show(errors.ToString());
-                return;
-            }
+            //adaptor1.SelectCommand = comand1;
+            //adaptor1.Fill(dataset1, "0");
+            //adaptor1.Fill(dt);
+            //int count1 = dataset1.Tables[0].Rows.Count;
+            //if (count1 > 0)
+            //{
+            //    connection1.Close();
+            //    Manager.MainFrame.Navigate(new MainPage());
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Не правильный логин или пароль");
+            //    Loging.Clear();
+            //    Passwordg.Clear();
+            //}
+            //StringBuilder errors = new StringBuilder();
 
-            if (_currentUser.USER_ID == 0 && _currentUser.USER_PASSWORD == ConfPass)
-                VOLGA_EAS_DBEntities.GetContext().USERS.Add(_currentUser);
+            //if (string.IsNullOrWhiteSpace(_currentUser.USER_EMAIL))
+            //    errors.AppendLine("Укажите электронную почту");
 
-            try
-            {
-                VOLGA_EAS_DBEntities.GetContext().SaveChanges();
-                MessageBox.Show("Информация сохранена");
-                Manager.MainFrame.GoBack();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message.ToString());
-            }
+            //if (string.IsNullOrWhiteSpace(_currentUser.USER_NAME))
+            //    errors.AppendLine("Укажите никнейм");
+
+            //if (string.IsNullOrWhiteSpace(_currentUser.USER_PASSWORD))
+            //    errors.AppendLine("Укажите пароль");
+
+            //if (errors.Length > 0)
+            //{
+            //    MessageBox.Show(errors.ToString());
+            //    return;
+            //}
+
+            //if (_currentUser.USER_ID == 0 && _currentUser.USER_PASSWORD == ConfPass)
+            //    VOLGA_EAS_DBEntities.GetContext().USERS.Add(_currentUser);
+
+            //try
+            //{
+            //    VOLGA_EAS_DBEntities.GetContext().SaveChanges();
+            //    MessageBox.Show("Информация сохранена");
+            //    Manager.MainFrame.GoBack();
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message.ToString());
+            //}
         }
 
         private void TextBox_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
